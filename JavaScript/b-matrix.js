4231fb45f2587d9c997b6c085808cbd9dad4c392
Eduard <eduard.keem@gmail.com>
2022-09-08 20:11:06 +0600

    8.9.22

--- JavaScript/b-matrix.js
+++ JavaScript/b-matrix.js
@@ -7,16 +7,24 @@ const matrix = [
   [-6, 4, 8, 2, 0],
 ];
 
-const max = (a, b) => (a > b ? a : b);
+const max = (a, b) => (a > b ? a : b);  // Вывод наибольшего числа
 
 const res = matrix
-  .map((row) => row.reduce(max))
-  .reduce((acc, rowMax) => acc + rowMax);
+  .map((row) => row.reduce(max))              // Вычисляем максимальное число из подмассива с помощью функции
+  .reduce((acc, rowMax) => acc + rowMax);     // Складываем максимальные числа из подмассивов
 
 console.log(res);
 
-for (const row of matrix) {
-  for (const item of row) {
-    console.log(item);
+for (const i in matrix) {
+  const row = matrix[i];
+  for (const j in row) {
+    const col = row[j];
+    console.log(i, j, col);
   }
 }
+
+// for (const row of matrix) {
+//   for (const item of row) {
+//     console.log(item);
+//   }
+// }
